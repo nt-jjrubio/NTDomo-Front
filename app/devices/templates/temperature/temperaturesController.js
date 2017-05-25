c6ce0a5537182ebaca790c450555a8f7c8de755f
Juan José Rubio Iglesias <d4rkh0s7@gmail.com>
2017-05-25 18:15:24 +0200

    CSS and other fixes

--- app/devices/templates/temperature/temperaturesController.js
+++ app/devices/templates/temperature/temperaturesController.js
@@ -9,24 +9,34 @@
         .module('NTDomo')
         .controller('TemperaturesController', TemperaturesController);
 
-    TemperaturesController.$inject = ['$rootScope'];
+    TemperaturesController.$inject = ['$rootScope', '$interval'];
 
-    function TemperaturesController($rootScope) {
-
+    function TemperaturesController($rootScope, $interval) {
         console.log("Entra en el controlador de temperaturas");
         var vm = this;
+        vm.reloadTime = 1000;
         vm.device = $rootScope.device;
-
+        vm.refreshTimes = [500, 1000, 2000, 5000, 10000];
         $rootScope.$on('deviceSelected', function (event, data) {
            vm.device = data;
             console.log('deviceSelected ', vm.device);
         });
+        vm.value=0;
+
+        // Function to query
+        var reloadQuery = function (){
+            vm.value++;
+        };
+
+
+        $interval(reloadQuery, vm.reloadTime);
 
 
         vm.whatDevice = function(){
             console.log("Device selected = ", vm.device);
         }
 
+        
     }
 
 
