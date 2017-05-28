ec6140d44e4b056ea81efafc105589c53a81b86b
Juan José Rubio <admin@tallernt.es>
2017-05-28 12:45:33 +0200

    i2cRequests read analog 0 values and write pin 0 Switch Working

--- app/devices/templates/temperature/temperaturesController.js
+++ app/devices/templates/temperature/temperaturesController.js
@@ -10,22 +10,62 @@
         console.log('Entra en el controlador de temperaturas');
         var vm = this;
         vm.switch = [];
+
         vm.reloadTime = 1000;
         vm.device = $rootScope.device;
         vm.refreshTimes = [500, 1000, 2000, 5000, 10000];
         $rootScope.$on('deviceSelected', function (event, data) {
            vm.device = data;
+           vm.switchStatus();
             console.log('deviceSelected ', vm.device);
         });
         vm.value=0;
+        vm.switchStatus = function (){
+            console.log('entra en la consulta?');
+            var query = {};
+            query.dev = vm.device.address;
 
+            // Command to check digital pin 2 status
+            query.cmd = 52;
+            /*if (ENV.env === 'dev') {
+             //query.idA = '2';
+             } else{
+             query.dev = '0x08';
+             }*/
+
+            I2CRequestService.query(query, function (data) {
+
+                if(data.value){
+                    vm.switch[0] = true;
+                }else{
+                    vm.switch[0] = false;
+                }
+
+                console.log('switchStatus', data.value);
+            }, function (err) {
+                console.error(err);
+                //console.error('error!');
+                var msg = '';
+                switch (err.status) {
+                    case 404:
+                        msg = 'Error 404';//TranslationService.err404; '
+                        break;
+                    case 500:
+                        msg = 'Error 500'; //TranslationService.err500;
+                        break;
+                    default:
+                        msg = 'Error default'; //TranslationService.errDefault;
+                        break;
+                }
+            });
+
+
+        };
         // Function to query
         var reloadQuery = function (){
             //vm.value++;
-
-
             /** Devices query **/
-            console.log(vm.device);
+            //console.log(vm.device);
             var query = {};
             query.dev = vm.device.address;
             // 40 is command for read Analog PIN 0
@@ -86,7 +126,11 @@
 
             I2CRequestService.query(query, function (data) {
 
-                vm.switch[0] = data.value;
+                if(data.value){
+                    vm.switch[0] = true;
+                }else{
+                    vm.switch[0] = false;
+                }
 
                 console.log('switchOnOff', vm.query);
             }, function (err) {
